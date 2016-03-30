dec5aa40133f80291e88eac5f5c4caa6aada0541
chiqua25 <juwujaren@gmail.com>
2016-03-29 21:09:33 -0400

    Feature: - Updated Tables and StoredProcedures to use UserName instead of surrogate key

--- HandymanTools.Database/HandymanTools.Database/StoredProcedures/usp_GetReservationsByCustomerId.sql
+++ HandymanTools.Database/HandymanTools.Database/StoredProcedures/usp_GetReservationsByCustomerId.sql
@@ -6,10 +6,6 @@ AS
 		(
 			(
 				SELECT ', ' + Tool.AbbrDescription 
-				--FROM Tool t
-				--INNER JOIN ReservationTool rt ON t.ToolId = rt.ToolId
-				--WHERE Reservation.ReservationNumber = rt.ReservationNumber
-				--GROUP BY Tool.AbbrDescription
 			), 1, 1, ''
 		) AS Tools,
 		Reservation.StartDate, 
@@ -21,10 +17,10 @@ AS
 	FROM Reservation 
 		INNER JOIN ReservationTool ON Reservation.ReservationNumber = ReservationTool.ReservationNumber
 		INNER JOIN Tool ON Tool.ToolId = ReservationTool.ToolId 
-		LEFT JOIN Clerk AS PickupClerk ON Reservation.PickupClerkId = PickupClerk.UserId
-		LEFT JOIN [User] AS PickupClerkUser ON PickupClerk.UserId = PickupClerkUser.UserId
-		LEFT JOIN Clerk AS DropOffClerk ON Reservation.DropOffClerkId = DropOffClerk.UserId
-		LEFT JOIN [User] AS DropOffClerkUser ON DropOffClerk.UserId = DropOffClerkUser.UserId 
+		LEFT JOIN Clerk AS PickupClerk ON Reservation.PickupClerkId = PickupClerk.UserName
+		LEFT JOIN [User] AS PickupClerkUser ON PickupClerk.UserName = PickupClerkUser.UserName
+		LEFT JOIN Clerk AS DropOffClerk ON Reservation.DropOffClerkId = DropOffClerk.UserName
+		LEFT JOIN [User] AS DropOffClerkUser ON DropOffClerk.UserName = DropOffClerkUser.UserName 
 	WHERE Reservation.CustomerId = @CustomerId 
 	GROUP BY Reservation.ReservationNumber,
 		Tool.AbbrDescription, 
--- HandymanTools.Database/HandymanTools.Database/StoredProcedures/usp_Report_ClerkProgress.sql
+++ HandymanTools.Database/HandymanTools.Database/StoredProcedures/usp_Report_ClerkProgress.sql
@@ -4,17 +4,17 @@
 AS
 	SELECT [User].FirstName, [User].LastName, TotalPickups, TotalDropoffs, (TotalPickups + TotalDropoffs) AS TotalSum 
 	FROM Clerk
-		INNER JOIN [User] ON [User].UserId = Clerk.UserId
+		INNER JOIN [User] ON [User].UserName = Clerk.UserName
 		INNER JOIN (
 			SELECT Reservation.PickupClerkId, count(Reservation.PickupClerkId) AS TotalPickups FROM Reservation
 
 			WHERE DATEPART(mm, Reservation.StartDate) = @Month AND DATEPART(yyyy, Reservation.StartDate) = @Year
 			GROUP BY Reservation.PickupClerkId
-		) AS Pickups on Pickups.PickupClerkId = Clerk.UserId 
+		) AS Pickups on Pickups.PickupClerkId = Clerk.UserName 
 		INNER JOIN (
 			SELECT Reservation.DropOffClerkId, count(Reservation.DropOffClerkId) AS TotalDropoffs FROM Reservation
 			WHERE DATEPART(mm, Reservation.StartDate) = @Month AND DATEPART(yyyy, Reservation.StartDate) = @Year
 			GROUP BY Reservation.DropOffClerkId
-		) AS Dropoff on Dropoff.DropOffClerkId = Clerk.UserId
+		) AS Dropoff on Dropoff.DropOffClerkId = Clerk.UserName
 	ORDER BY TotalPickups, TotalDropoffs
 RETURN 0
